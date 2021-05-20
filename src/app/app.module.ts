92898b7b3e5720485e81d9d5c8291c4e4f9ec24f
cchuc000 <cristofer.chuc91@student.qcc.cuny.edu>
2021-05-20 12:43:00 -0400

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
@@ -10,7 +11,7 @@ import { HelloComponent } from './hello.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, HttpClientModule ],
+  imports:      [ BrowserModule,  AppRoutingModule, FormsModule, HttpClientModule ],
   declarations: [ AppComponent, HelloComponent, SearchBarComponent ],
   bootstrap:    [ AppComponent ]
 })
